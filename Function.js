0601b21b80069550c8a54d4c0b8bb76d37e8ec31
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2024-08-22 11:19:10 +0530

    function

--- Function.js
+++ Function.js
@@ -3,7 +3,18 @@
 Multiply = (a,b) => a * b;
 console.log(Multiply(5,7));
 
+
+
 //IndexOf method
 
 var index = [12,5,8,130,44].indexOf(130);
 console.log(index);
+
+// Concat method and toString method
+
+var num = [1,2,3];
+var num1 = [4,5,6];
+var num2 = num.concat(num1);
+console.log(num2);
+console.log(num2.toString());
+
